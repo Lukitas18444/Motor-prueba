6eedefd7b09f067eea27e2ab6538005dbde29c50
Lucas <caballerolucas18@gmail.com>
2026-06-10 12:58:54 -0300

    prueba netlfy 3

--- Motor-prueba/src/App.jsx
+++ Motor-prueba/src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx' 
 import Home from './pages/Home.jsx'; 
 import Productos from './pages/Productos.jsx';
